9e91339b8ae921bad2383c4088d18f470d704c9a
shimtaewoo <youdig@naver.com>
2022-10-11 18:30:43 +0900

    production 0 test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express1--postback' });
+  res.render('index', { title: 'Express1--ta!!!' });
 });
 
 module.exports = router;
